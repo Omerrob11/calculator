8a130500a239c74251400d20f3c0af7a9b728693
Omer Miloh <omerrob11@gmail.com>
2022-02-02 19:26:45 +0200

    adding script file

---
+++ script.js
@@ -0,0 +1,9 @@
+// - Get user inputs
+//     - Get user numbers
+//     - Get user operation
+//     - Get user sum
+//     - Get user clear
+// - Create a function operate, takes the user inputs 2 numbers, and an operator sign
+// - Create 4 operations functions
+// - Get the display div
+// - Print the calculation to the div
